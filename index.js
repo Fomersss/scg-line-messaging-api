9f26e8afc60e9083c8d53d4834e487b85630f380
Fomersss <ratchanonwad@gmail.com>
2019-10-20 12:29:08 +0700

    edit config

--- index.js
+++ index.js
@@ -14,25 +14,19 @@ admin.initializeApp({
 });
 
 
-const configLine = {
-  channelAccessToken: '41xVYHPUA4RAnSlm46Q1sXUCzL2h/laISBgNuL1cdY2fFhAYX0z3anpFYqSb1vzDilmANUbrSzge3q1Qt47Pck3sJVUJ9XQPixycEtWjZpcTDAigkuYeZTCaJA69Ru4yJsTUBOKUeXW0p/JKZou3PwdB04t89/1O/w1cDnyilFU=',
-  channelSecret: '2c573d3d3f983c9261e5e1fcc9729b70',
-};
-
-
 // create LINE SDK client
-const client = new line.Client(configLine);
+const client = new line.Client(config);
 
 // create Express app
 const app = express();
 
 
 // register a webhook handler with middleware
-app.post('/callback', line.middleware(configLine), (req, res) => {
+app.post('/callback', line.middleware(config), (req, res) => {
   Promise
     .all(req.body.events.map((event) => {
       // for verify line webhook
-      if (event.source.userId === 'Udeadbeefdeadbeefdeadbeefdeadbeef') {
+      if (event.source.userId === config.userId) {
         return;
       }
       return handleEvent(event,req)
